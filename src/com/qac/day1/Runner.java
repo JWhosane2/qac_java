663fe4c2a14d82f17175626a178764853c76225a
Jahangir <jahangir.hussain@academytrainee.com>
2020-04-07 17:21:18 +0100

    this is my first java

---
+++ src/com/qac/day1/Runner.java
@@ -0,0 +1,19 @@
+package com.qac.day1;
+
+public class Runner {
+	
+	//this is a single line comment
+	/*
+	 * this is a
+	 * multi line
+	 * comment
+	 */
+	public static void main(String args[]) {
+	int age=23; //this is a variable
+	
+	
+	System.out.println("hello world");
+	System.out.println("i am " + age);
+	}
+
+}
